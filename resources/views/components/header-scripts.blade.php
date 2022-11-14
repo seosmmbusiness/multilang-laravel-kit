159418ca3ca0a1d876116828d3b415d40a4597a5
Kirill Ivanov <kirill84ivanov@gmail.com>
2022-11-14 19:47:39 +0300

    workign with GA

--- resources/views/components/header-scripts.blade.php
+++ resources/views/components/header-scripts.blade.php
@@ -1,7 +1,3 @@
 <script>
     console.log("Script runs before </head>")
 </script>
-<!-- For example how You can put your GA4 tag here -->
-<!-- Google tag (gtag.js) -->
-<script src="https://www.googletagmanager.com/gtag/js?id=G-4YEXWD71MJ" defer></script>
-<script src="{{ mix('js/ga.js') }}" defer></script>
